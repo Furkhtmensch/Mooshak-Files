efb1bd36590e92fad3ca45a3caf1661f46f4603b
Fürkhtmensch <42923229+Furkhtmensch@users.noreply.github.com>
2023-10-25 16:04:34 +0100

    B.py

--- B.py
+++ B.py
@@ -1,13 +1,10 @@
 C, N = map(int , input().split())
 counter = 0
-lis = []
 for i in range(N):
     P = int(input())
     D = input()
     if P <= C:
-        lis.append(D)
+        print(D)
         C -= P
         counter += P
-for i in lis:
-    print(i)
 print(counter, C)
